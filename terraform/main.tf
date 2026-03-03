1dc3ac2e2bdd3eba06d63da91b27ad1ceb08a22b
ramakrishnaredd95-stack <ramakrishnaredd95@gmail.com>
2026-03-04 02:16:25 +0530

    Update storage account name in Terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "flipkart-tfstate-rg"
-    storage_account_name = "flipkarttfstate"
+    storage_account_name = "flipkarttfstate124"
     container_name       = "tfstate"
     key                  = "flipkartapp.terraform.tfstate"
   }
