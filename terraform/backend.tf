59444922c1b189a07b2f7f3117d92e5c86f4dcfc
FlipkartApp Dev <flipkartapp@dev.com>
2026-03-05 15:56:09 +0530

    terraform issue resolved1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "flipkart-tfstate-rg"
-    storage_account_name = "flipkarttfstate124"
+    storage_account_name = "flipkarttfstate134"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
