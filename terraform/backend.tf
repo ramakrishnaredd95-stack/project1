136a13879accd985803c0de5bc0b27abfd2bead8
FlipkartApp Dev <flipkartapp@dev.com>
2026-03-07 11:38:18 +0530

    new changes are added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rsg-storage-account"
-    storage_account_name = "storageaccount9802"
+    storage_account_name = "flipkarttfstate356"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
